206e763df2a69f4cb4cda2700a9c5ae13f0b6e13
Mark <mark.oconnor14@gmail.com>
2025-09-29 15:45:25 +0800

    update seeder with seed_users function and change classes/students to reflect addition of users

--- scripts/seed.py
+++ scripts/seed.py
@@ -6,42 +6,63 @@ from faker import Faker
 from structs import ClassRow
 
 
-def seed_classes() -> None:
+def seed_users() -> None:
     data = [
-        ("Class A",),
-        ("Class B",),
-        ("Class C",),
-        ("Class D",),
-        ("Class E",),
+        ("test1@testmail.com", "test1234"),
+        ("test2@testmail.com", "test1234"),
+        ("test3@testmail.com", "test1234"),
+        ("test4@testmail.com", "test1234"),
+        ("test5@testmail.com", "test1234")
     ]
+
     with sqlite3.connect("db.sqlite3") as conn:
+        conn.execute("PRAGMA foreign_keys = ON;")
         cursor = conn.cursor()
         try:
-            cursor.executemany("INSERT INTO classes (name) VALUES (?);", data)
+            cursor.executemany("INSERT INTO users (email, password) VALUES (?, ?)", data)
+        except Exception as e:
+            print(f"an error occured inserting users: {e}")
+
+    print("Successfully seeded DB with users")
+
+
+def seed_classes() -> None:
+    data = [
+        ("Canary", 1),
+        ("Crow", 1),
+        ("Bluejay", 2),
+        ("Goose", 2),
+        ("Ostrich", 3),
+    ]
+
+    with sqlite3.connect("db.sqlite3") as conn:
+        conn.execute("PRAGMA foreign_keys = ON;")
+        cursor = conn.cursor()
+        try:
+            cursor.executemany("INSERT INTO classes (name, owner_id) VALUES (?, ?);", data)
         except Exception as e:
             print(f"An error occured inserting classes: {e}")
-        cursor.close()
-        conn.commit()
 
-    print("DB seeded with classes")
+    print("Successfully seeded DB with classes")
 
 def seed_students():
     with sqlite3.connect("db.sqlite3") as conn:
+        conn.execute("PRAGMA foreign_keys = ON;")
         cursor = conn.cursor()
         cursor.execute("SELECT id, name FROM classes;")
         class_groups = [ClassRow(*row) for row in cursor.fetchall()]
+
         fake = Faker()
         students = []
         for class_group in class_groups:
-            for i in range(random.randint(10, 15)):
+            for _ in range(random.randint(10, 15)):
                 students.append((fake.name(), class_group.id))
         try:
             cursor.executemany("INSERT INTO students (name, class_id) VALUES (?, ?)", students)
-            conn.commit()
         except Exception as e:
             print(f"An error occured inserting students: {e}")
 
-    print("DB seeded with students")
+    print("Successfully seeded DB with students")
 
 def list_classes():
     connection = sqlite3.connect("db.sqlite3")
@@ -55,7 +76,3 @@ def list_classes():
         print(f"row: {row}")
         print(f"id: {row[0]}")
         print(f"name: {row[1]}")
-
-# seed_classes()
-# seed_students()
-# list_classes()
