56cc0eac321a42356d32d6291c0c14aeb45d11a7
Mark <mark.oconnor14@gmail.com>
2025-09-30 12:12:19 +0800

    create session structs

--- structs.py
+++ structs.py
@@ -9,3 +9,7 @@ ClassOwnerRow = namedtuple("ClassRow", ["id", "name", "owner_id"])
 StudentRow = namedtuple("StudentRow", ["id", "name", "class_id"])
 
 StudentCreate = namedtuple("StudentCreate", ["name", "class_id"])
+
+SessionRow = namedtuple("SessionRow", ["id", "token", "user_id", "expires_at"])
+
+SessionCreate = namedtuple("SessionCreate", ["token", "user_id", "expires_at"])
